a413426837eae524e521d7ecb10ed7d1f90e764e
Sebastian <sebastian.geheeb@gmx.de>
2020-12-11 21:54:36 +0100

    PlayerController

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(camera.transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        rb.AddForce(camera.transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+}
